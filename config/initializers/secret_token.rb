932cd3b6c6e12d99516585d8bbc890abd733b0a1
hung <xuanhung.ta@gmail.com>
2013-05-08 23:20:46 +0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemTesting::Application.config.secret_token = '4d85d29d8a3a4ce80bb8db45871251a63317c52e5a12cc6a7890da47e0d34dfb121b254291ed3351180a5ca4e3cd5060e6f9171256a128133e43aa7762cd2fd9'
